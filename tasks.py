466b2bb75ee9f82df4731dde912380a9c95ced8a
Sergey Romanov <xxsmotur@gmail.com>
2015-07-15 23:52:12 +0500

    Add new RF task

--- tasks.py
+++ tasks.py
@@ -2,7 +2,9 @@ import celery
 from sklearn import svm
 from sklearn.externals import joblib
 from sklearn.datasets import load_iris
+from sklearn.ensemble import RandomForestCLassifier
 import os
+import datetime
 
 app = celery.Celery("ml")
 app.config_from_object('celeryconfig')
@@ -14,6 +16,18 @@ def compute_model():
     model.fit(data.data, data.target)
     joblib.dump(model, '../svmmodel.pkl', compress=9)
 
+
+@app.task
+def compute_rfmodel(params, data):
+    '''
+    Args:
+        params - contain dict of username and other params
+    '''
+    username = params['username']
+    strtime = datetime.datetime.now().strftime('%Y_%m_%d_%H_%M_%M_%S')
+    model = RandomForestClassifier()
+    model.fit(data)
+    joblib.dump(model, '../{0}_{1}_rfmodel.pkl'.format(username, strtime), compress=9)
 
 @app.task()
 def get_result(data):
