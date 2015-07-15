21c4bc1ce62f6fad57228c03db6346b649b1ae56
Sergey Romanov <xxsmotur@gmail.com>
2015-07-15 23:25:13 +0500

    Add comment

--- client.py
+++ client.py
@@ -1,7 +1,8 @@
 import requests
 
-
+#Example of sending test data
 def send_data_for_predict(data):
     r = requests.get('http://127.0.0.1:8080', params={'data': data})
-    print(r)
+    print("DATA: ", r)
 
+send_data_for_predict([0.4,0.5,0.7,0.1])
