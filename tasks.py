d6b23a7c37fd86d7230693af9c53bf31551932f0
Sergey Romanov <xxsmotur@gmail.com>
2015-07-16 01:34:04 +0500

    update tasks

--- tasks.py
+++ tasks.py
@@ -2,7 +2,7 @@ import celery
 from sklearn import svm
 from sklearn.externals import joblib
 from sklearn.datasets import load_iris
-from sklearn.ensemble import RandomForestCLassifier
+from sklearn.ensemble import RandomForestClassifier
 import os
 import datetime
 
@@ -10,11 +10,14 @@ app = celery.Celery("ml")
 app.config_from_object('celeryconfig')
 
 @app.task
-def compute_model():
+def compute_model(params):
     data = load_iris()
+    username = params['username']
+    strtime = getStrTime()
     model = svm.SVC()
     model.fit(data.data, data.target)
-    joblib.dump(model, '../svmmodel.pkl', compress=9)
+    joblib.dump(model, '../{0}_{1}_svmmodel.pkl'.format(username, strtime), compress=9)
+    return 1
 
 
 @app.task
@@ -24,7 +27,7 @@ def compute_rfmodel(params, data):
         params - contain dict of username and other params
     '''
     username = params['username']
-    strtime = datetime.datetime.now().strftime('%Y_%m_%d_%H_%M_%M_%S')
+    strtime = getStrTime()
     model = RandomForestClassifier()
     model.fit(data)
     joblib.dump(model, '../{0}_{1}_rfmodel.pkl'.format(username, strtime), compress=9)
@@ -35,3 +38,10 @@ def get_result(data):
         return {'result':'', 'error': 'File with model is not found'}
     model = joblib.load('../svmmodel.pkl')
     return {'result': str(model.predict(data)[0]), 'error': ''}
+
+
+def getStrTime():
+    ''' Return current time in str format
+    '''
+    return datetime.datetime.now().strftime('%Y_%m_%d_%H_%M_%M_%S')
+
